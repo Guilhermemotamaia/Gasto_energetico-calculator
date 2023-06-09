8b847e373ca429e41325674b39e597c8e626bb46
Guilherme Mota <g.m.maia@outlook.com>
2023-06-09 17:09:42 -0400

    second version
    now the user can choose between doing another calculation or ending the program

--- myprojects01.py
+++ myprojects01.py
@@ -1,25 +1,29 @@
+from time import  sleep
 print('VAMOS CALCULAR O GASTO ENERGÉTICO!!')
+while True:
+    sexo = int(input('Você e Homem [1] ou Mulher [2]?'))
+    while sexo not in [1, 2]:
+        print("Modo de dificuldade inválido. Escolha novamente:")
+        sexo = int(input('Você é Homem [1] ou Mulher [2]?'))
 
-sexo = int(input('Você e Homem [1] ou Mulher [2]?'))
+    #Verifica o peso e tranformar strg em float
+    peso = str(input('Digite o seu peso em Kg:'))
+    peso = str(peso).replace(',','.')
+    numeric_transformer = float(peso)
 
-while sexo not in [1, 2]:
-    print("Modo de dificuldade inválido. Escolha novamente:")
-    sexo = int(input('Você é Homem [1] ou Mulher [2]?'))
+    altura = int(input('Dgite a sua altura em cm:'))
 
-#Verifica o peso e tranformar strg em float
-peso = str(input('Digite o seu peso em Kg:'))
-peso = str(peso).replace(',','.')
-numeric_transformer = float(peso)
+    idade = int(input('Digite a sua idade:'))
 
-altura = int(input('Dgite a sua altura em cm:'))
+    if sexo == 1:
+        caloric_expenditure = 66 + (13.7 * numeric_transformer) + (5 * altura) - (6.5 * idade)
+        print(f'O seu gasto energético e de {caloric_expenditure} calorias.')
 
-idade = int(input('Digite a sua idade:'))
-
-if sexo == 1:
-    caloric_expenditure = 66 + (13.7 * numeric_transformer) + (5 * altura) - (6.5 * idade)
-    print(f'O seu gasto energético e de {caloric_expenditure} calorias.')
-
-else:
-    caloric_expenditure = 655 + (9.6 * numeric_transformer) + (1.8 * altura) - (4.7 * idade)
-    print(f'O seu gasto energético e de {caloric_expenditure} calorias.')
+    else:
+        caloric_expenditure = 655 + (9.6 * numeric_transformer) + (1.8 * altura) - (4.7 * idade)
+        print(f'O seu gasto energético e de {caloric_expenditure} calorias.')
+    sleep(0.6)
+    continuar = int(input('Deseja fazer outro cálculo do gasto enérgetico? [1] SIM [2] NÃO.'))
+    if continuar == 2:
+        break
 
